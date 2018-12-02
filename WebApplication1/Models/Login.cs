5667e006fad36713e0acaacab530f7227fbe910f
Florina-Maria Bindila <1066549@ucn.dk>
2018-12-02 15:29:26 +0100

    3.0

---
+++ WebApplication1/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Login
+    {
+        public int ID { get; set; }
+        public string Usename { get; set; }
+        public string Password { get; set; }
+        public int Accesability { get; set; }
+    }
+}
