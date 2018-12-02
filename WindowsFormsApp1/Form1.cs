f8d80d05314cf4acb78baa8f1c39ae448d1b090e
Florina-Maria Bindila <1066549@ucn.dk>
2018-12-02 15:55:15 +0100

    3.1

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,30 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Label(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userName_Label(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userName_TextBoxChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
